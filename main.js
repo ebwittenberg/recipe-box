54011aad7910175949eccc3e9c6d22d913c3963e
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-03 16:38:00 -0500

    recipe names are now stored in a dynamic dropdown menu

--- main.js
+++ main.js
@@ -2,11 +2,31 @@
 // saves the recipe name in a variable
 function recipeName() {
     // save recipe name (value of user input)
-    let recipeName = document.querySelector(".name").value; 
-    console.log(recipeName);
+    let inputRecipeName = document.querySelector(".name");
+    let recipeName = inputRecipeName.value;
+    
+    // clears recipe name input box for next entry
+    inputRecipeName.value = '';
+    
+    // calls add recipe function, passing it the recipe name
+    addRecipe(recipeName);
 }
 
 // when submit button is clicked, run recipeName function
 let submit = document.querySelector('.submit-button');
 
 submit.addEventListener('click', recipeName);
+
+// after user enters recipe name, populate that recipe name into dropdown menu
+
+let dropdown = document.querySelector('.recipe-dropdown');
+
+function addRecipe(recipeName) {
+
+    // create new option element that will go into dropdown
+    let option = document.createElement('option');
+    // text content of option is the recipe name
+    option.textContent = recipeName;
+    // append the recipe name option into the dropdown
+    dropdown.appendChild(option);
+}
