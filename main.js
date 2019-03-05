ab999328fe11a28c1e653e7a54ac96e4350e3bcc
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-04 19:51:42 -0500

    puts add recipe name functions in their own file

--- main.js
+++ main.js
@@ -1,38 +1,9 @@
 let body = document.querySelector('body');
-// saves the recipe name in a variable
-function recipeName() {
-    // save recipe name (value of user input)
-    let inputRecipeName = document.querySelector(".name");
-    let recipeName = inputRecipeName.value;
-    
-    // clears recipe name input box for next entry
-    inputRecipeName.value = '';
 
-    // calls add recipe function, passing it the recipe name
-    addRecipe(recipeName);
-}
-
-// when submit button is clicked, run recipeName function
 let submit = document.querySelector('.submit-button');
-
 submit.addEventListener('click', recipeName);
 
-// after user enters recipe name, populate that recipe name into dropdown menu
-
 let dropdown = document.querySelector('.recipe-dropdown');
-
-function addRecipe(recipeName) {
-
-    // create new option element that will go into dropdown
-    let option = document.createElement('option');
-    // text content of option is the recipe name
-    option.textContent = recipeName;
-    option.classList.add(recipeName);
-    // append the recipe name option into the dropdown
-    dropdown.appendChild(option);
-
-    createIngredientBox(recipeName);
-}
 
 // save ingredients user enters in a unordered list
 let saveIngredientButton = document.querySelector('.save-ingredient');
